f2e3806c33bead3c0c72c51201cbb870fd1261aa
Ahmed Abdelaal <ahmed.abdellaoue@gmail.com>
2020-08-12 16:49:33 +0200

    Changes in the form

--- src/Form/BusinessType.php
+++ src/Form/BusinessType.php
@@ -5,6 +5,12 @@ namespace App\Form;
 use App\Entity\Business;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\CountryType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class BusinessType extends AbstractType
@@ -12,21 +18,21 @@ class BusinessType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('branch')
-            ->add('nrEmployees')
-            ->add('strNameNr')
-            ->add('city')
-            ->add('zipcode')
-            ->add('country')
-            ->add('eMail')
-            ->add('webpage')
-            ->add('telNr')
-            ->add('logo')
-            ->add('image')
-            ->add('description')
-            ->add('moto')
-            ->add('gmaploc')
+            ->add('name', TextType::class, ['help' => 'Required', 'required' => true, 'label' => 'Business Name:'])
+            ->add('branch', TextType::class, ['help' => 'Required', 'required' => true, 'label' => 'Business Branch:'])
+            ->add('nrEmployees', IntegerType::class, ['help' => 'Optional', 'required' => false])
+            ->add('strNameNr', TextType::class, ['help' => 'Optional', 'required' => false, 'label' => 'Street name & Nr.:'])
+            ->add('city', TextType::class, ['help' => 'Optional', 'required' => false])
+            ->add('zipcode', IntegerType::class, ['help' => 'Optional', 'required' => false])
+            ->add('country', CountryType::class, ['required' => false])
+            ->add('eMail', EmailType::class, ['help' => 'Optional', 'required' => false, 'label' => 'E-mail address:'])
+            ->add('webpage', UrlType::class, ['help' => 'Optional', 'required' => false, 'label' => 'Website:'])
+            ->add('telNr', TextType::class, ['help' => 'Optional', 'required' => false, 'label' => 'Contact Tel. Nr.:'])
+            ->add('logo', TextType::class, ['help' => 'Optional', 'required' => false, 'label' => 'Company Logo:'])
+            ->add('image', TextType::class, ['help' => 'Optional', 'required' => false, 'label' => 'Teaser:'])
+            ->add('description', TextType::class, ['help' => 'Optional', 'required' => false, 'label' => 'About the company:'])
+            ->add('moto', TextType::class, ['help' => 'Optional', 'required' => false, 'label' => 'Motto:'])
+            ->add('gmaploc', TextType::class, ['help' => 'Optional', 'required' => false, 'label' => 'Location:'])
         ;
     }
 
--- src/Form/PostType.php
+++ src/Form/PostType.php
@@ -5,6 +5,9 @@ namespace App\Form;
 use App\Entity\Post;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class PostType extends AbstractType
@@ -12,19 +15,19 @@ class PostType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
-            ->add('subtitle')
-            ->add('datePublished')
-            ->add('authorFn')
-            ->add('authorLn')
-            ->add('imageMain')
-            ->add('imageMiddle')
-            ->add('imageBottom')
-            ->add('excerpt')
-            ->add('introText')
-            ->add('mainText')
-            ->add('outroText')
-            ->add('post_type')
+            ->add('title', TextType::class, ['help' => 'Required', 'required' => true, 'label' => 'Blog Title:'])
+            ->add('subtitle', TextType::class, ['help' => 'Optional', 'required' => false, 'label' => 'Subtitle:'])
+            ->add('datePublished', DateType::class, ['help' => 'Required', 'widget' => 'single_text', 'format' => 'yyyy-MM-dd', 'required' => true])
+            ->add('authorFn', TextType::class, ['help' => 'Optional', 'required' => false, 'label' => 'First Name:'])
+            ->add('authorLn', TextType::class, ['help' => 'Optional', 'required' => false, 'label' => 'Last Name:'])
+            ->add('imageMain', TextType::class, ['help' => 'Required', 'required' => true, 'label' => 'Teaser Image:'])
+            ->add('imageMiddle', TextType::class, ['help' => 'Optional', 'required' => false, 'label' => 'Middle Image:'])
+            ->add('imageBottom', TextType::class, ['help' => 'Optional', 'required' => false, 'label' => 'Bottom Image:'])
+            ->add('excerpt', TextareaType::class, ['help' => 'Optional, max 500 characters', 'required' => false, 'label' => 'Teaser Text:'])
+            ->add('introText', TextareaType::class, [ 'help' => 'Optional', 'required' => false, 'label' => 'Intro:'])
+            ->add('mainText', TextareaType::class, [ 'help' => 'Optional', 'required' => false, 'label' => 'Main Text:'])
+            ->add('outroText', TextareaType::class, [ 'help' => 'Optional', 'required' => false, 'label' => 'Outro:'])
+            ->add('post_type', TextareaType::class, [ 'help' => 'Optional', 'required' => false, 'label' => 'Type:'])
         ;
     }
 
